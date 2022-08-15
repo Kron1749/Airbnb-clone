78137197e5f89ce3cf88f79b5d37e2f3485fb282
Kron1749 <drakgr98@gmail.com>
2022-08-15 16:06:19 +0300

    Passing cards with props

--- src/App.js
+++ src/App.js
@@ -8,18 +8,7 @@ import cardsElements from "./data";
 
 export default function App() {
   const cards = cardsElements.map((card) => {
-    return (
-      <Card
-        key={card.id}
-        img={card.coverImg}
-        rating={card.stats.rating}
-        number_of_rating={card.stats.reviewCount}
-        location={card.location}
-        title={card.title}
-        price={card.price}
-        openSpots={card.openSpots}
-      />
-    );
+    return <Card key={card.id} card={card} />;
   });
   return (
     <div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,26 @@
 import React from "react";
 import Star from "../images/Star.png";
 export default function Card(props) {
+  console.log(props);
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.card.openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (props.location === "Online") {
+  } else if (props.card.location === "Online") {
     badgeText = "ONLINE";
   }
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img src={props.img} className="card--image" />
+      <img src={props.card.coverImg} className="card--image" />
       <div className="card--stats">
         <img src={Star} className="card--star" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.number_of_rating})</span>
-        <span className="gray">{props.country}</span>
+        <span>{props.card.stats.rating}</span>
+        <span className="gray">({props.card.stats.reviewCount})</span>
+        <span className="gray">{props.card.location}</span>
       </div>
-      <p className="card--title">{props.title}</p>
+      <p className="card--title">{props.card.title}</p>
       <p className="card--price">
-        <span className="bold">From ${props.price}</span> / person
+        <span className="bold">From ${props.card.price}</span> / person
       </p>
     </div>
   );
